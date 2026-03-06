5a80541b8282330f5c622543e0d9197686154d39
zxcwn <tires098@gmail.com>
2026-03-07 00:31:06 +0300

    add ignore list via -x option

--- seaker/main.py
+++ seaker/main.py
@@ -3,6 +3,7 @@ import re
 import os
 from pathlib import Path
 import json
+
 mail_regex = r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b'
 phone_regex = r'^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?\d{3}[\- ]?\d{2}[\- ]?\d{2}$'
 ipv4_regex = r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b'
@@ -25,14 +26,52 @@ def print_help():
         Usage: program [options]
 
         Options:
-          -h, --help        See this message
-          -i DIR, --input DIR  Pick root directory (default: .)
+          -h, --help               See this message
+          -i DIR, --input DIR      Pick root directory (default: .)
+          -x DIR, --ignore DIR     Ignore directory
+          -x FILE, --ignore FILE   Ignore file
+          -x FILE1 -x FILE2 -x DIR The way to ignore multiple dirs/files
     """
     print(help_text)
+
+def find_regex(root_dir: str, ignore_list: list):
+    compiled_patterns = [(re.compile(p, re.IGNORECASE), dict_pattern[p][0], dict_pattern[p][1]) for p in patterns]
+    results = []
+    
+    for root, dirs, files in os.walk(root_dir):
+        dirs[:] = [d for d in dirs if os.path.join(root, d) not in ignore_list and d not in ignore_list]
+        for file in files:
+            file_path = Path(root) / file
+            file_path_str = str(file_path)
+            if file_path_str in ignore_list or file in ignore_list:
+                continue
+            try:
+                with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+                    lines = f.readlines()
+                    
+                    for line_num, line in enumerate(lines, 1):
+                        for pattern, description, level in compiled_patterns:
+                            matches = pattern.findall(line)
+                            if matches:
+                                for match in set(matches):
+                                    result = {
+                                        "file": str(file_path_str),
+                                        "line": str(line_num),
+                                        "description": description,
+                                        "snippet": match,
+                                        "level": level
+                                    }
+                                    results.append(result)
+                                    
+            except Exception as e:
+                pass
+    
+    print(json.dumps(results, indent=4, ensure_ascii=False))
 
 def main():
     argc = len(sys.argv)
     root_dir = "."
+    ignore_list = []
     if argc == 1:
         pass
     else:
@@ -46,42 +85,20 @@ def main():
                     root_dir = sys.argv[i + 1]
                     i += 2
                 else:
-                    print("DIR expected after -i (--input) option")
-                    print_help()
+                    print("DIR expected after -i (--input) option)")
+                    return
+            elif sys.argv[i] in ['-x', '--ignore']:
+                if i + 1 < argc:
+                    ignore_list.append(sys.argv[i + 1])
+                    i += 2
+                else:
+                    print("DIR or FILE expected after -x (--ignore) option")
                     return
             else:
                 print(f"Unknown option: {sys.argv[i]}")
-                print_help()
                 return
+    find_regex(root_dir, ignore_list)
     
-    compiled_patterns = [(re.compile(p, re.IGNORECASE), dict_pattern[p][0], dict_pattern[p][1]) for p in patterns]
-    results = []
-    
-    for root, dirs, files in os.walk(root_dir):
-        for file in files:
-            file_path = Path(root) / file
-            try:
-                with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
-                    lines = f.readlines()
-                    
-                    for line_num, line in enumerate(lines, 1):
-                        for pattern, description, level in compiled_patterns:
-                            matches = pattern.findall(line)
-                            if matches:
-                                for match in set(matches):
-                                    result = {
-                                        "file": str(file_path),
-                                        "line": str(line_num),
-                                        "description": description,
-                                        "snippet": match,
-                                        "level": level
-                                    }
-                                    results.append(result)
-                                    
-            except Exception as e:
-                pass
-    
-    print(json.dumps(results, indent=4, ensure_ascii=False))
 
 if __name__ == '__main__':
     main()
