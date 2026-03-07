204db5f1a3b65acfcd741c5e49c0964a09f31487
Fragoler <e.f.oleg@mail.ru>
2026-03-07 17:11:20 +0300

    prettify args

--- tui/src/main.py
+++ tui/src/main.py
@@ -1,41 +1,88 @@
-﻿from tui import tui
+﻿from tabnanny import check
+
+from tui import tui
 from install import install
+from ignore import ignore_path
+from pathlib import Path
 
 import sys
 import argparse
 import subprocess
 import json
 
-from ignore import ignore_path
 
 DEFAULT_CFG_PATH= ".nuclearss"
 
 def main():
-    parser = argparse.ArgumentParser(description="Process JSON input.")
+    parser = argparse.ArgumentParser(
+        description="NuclearSS: The Utility that protects the git repository from leaks of tokens, passwords, secrets, and other confidential information.",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog="""
+Examples:
+  nuclearss check /path    # Run default check /path directory
+  nuclearss report /path   # Generate PDF report for path
+  nuclearss tui data.json  # Launch TUI with JSON file
+  nuclearss install /path  # Install git hooks for selected repository
+  nuclearss install        # Install git hooks for current repository
+  nuclearss ignore /path   # Ignore all secrets in the specified path"""
+    )
 
-    parser.add_argument("command", nargs="?", choices=["report", "tui", "install", "ignore"], help="Command: report, tui, ignore <path>, or install <path>")
-    parser.add_argument("path", nargs="?")
-    parser.add_argument("-c", "--config", type=str, help=f"Specify config file (default: {DEFAULT_CFG_PATH})")
+    parser.add_argument("-c", "--config", default=f"{DEFAULT_CFG_PATH}",
+                        help="Config file path (default: %(default)s in target directory)")
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands", required=False)
+
+    _check = subparsers.add_parser("check",
+                          help="Run security scan on target path using nuclearss-seaker")
+    _check.add_argument("path", nargs="?", default=".",
+                        help="Target path to scan (default: current directory)")
+
+    report = subparsers.add_parser("report",
+                                   help="Generate PDF report from scan results")
+    report.add_argument("path", nargs="?", default=".",
+                        help="Target path to scan (default: current directory)")
+
+    tui_parser = subparsers.add_parser("tui",
+                                       help="Launch interactive TUI interface")
+    tui_parser.add_argument("path", nargs="?",
+                            help="JSON file path or read from stdin if omitted")
+
+    install_parser = subparsers.add_parser("install",
+                                           help="Install default config to path")
+    install_parser.add_argument("path", nargs="?", default=".",
+                                help="Installation path (default: current directory)")
+
+    ignore_parser = subparsers.add_parser("ignore",
+                                          help="Add path exclusion to config")
+    ignore_parser.add_argument("path", nargs=1,
+                               help="Path to ignore (e.g., 'node_modules/')")
     
     args = parser.parse_args()
+
+    if not args.command:
+        args.command = "check"
+        args = parser.parse_args([args.command])
     
     path = args.path if args.path else "."
-    config_file = args.config if args.config else DEFAULT_CFG_PATH
+    config_file = args.config if args.config else str(Path(path) / DEFAULT_CFG_PATH)
+    command = args.command if args.command else "check"
 
-    if args.command == "ignore":
+    if command == "ignore":
         if not args.path:
             print("Please specify a path to ignore.")
             sys.exit(1)
-            
-        ignore_path(args.path, config_file)
+        
+        for p in args.path:
+            ignore_path(p, config_file)
         sys.exit(0)
 
-    if args.command == "install":
-        install(args.path if args.path else ".")
+    if command == "install":
+        install(path)
         sys.exit(0)
 
+    # Get json data from nuclearss-seaker
     str_data = ""
-    if args.command != "tui":
+    if command in ["check", "report"]:
         result = subprocess.run(
             ["nuclearss-seaker", "-c", f"{config_file}", "-d", f"{path}"],
             stdout=subprocess.PIPE,
@@ -43,9 +90,8 @@ def main():
             text=True
         )
         str_data = result.stdout.strip()
-        if not str_data:
-            sys.exit(0)
-    else: # TUI mode enables
+
+    elif command == 'tui': 
         if args.path: 
             try:
                 with open(args.path, "r") as f:
@@ -55,10 +101,12 @@ def main():
                 sys.exit(1)
         else:
             str_data = sys.stdin.read()
-            if not str_data:
-                sys.exit(0)
+
+    # End process if no data provided
+    if not str_data:
+        sys.exit(0)
     
-    if args.command == "report":
+    if command == "report":
         result = subprocess.run(
             ["nuclearss-pdf"],
             input=str_data,
