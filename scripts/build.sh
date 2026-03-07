8a4ef6be86e6f4bbe6eb5eb1148d789fc90f9fb5
Fragoler <e.f.oleg@mail.ru>
2026-03-07 15:36:47 +0300

    move scripts to separate dir

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+
+pyinstaller --onefile \
+            --add-data "pdf-generator/background.png:." \
+            --name nuclearss-pdf \
+            pdf-generator/main.py
+            
+pyinstaller --onefile \
+            --add-data "seaker/patterns.json:." \
+            --name nuclearss-seaker \
+            seaker/src/main.py
+            
+pyinstaller --onefile \
+            --name nuclearss \
+            tui/src/main.py
